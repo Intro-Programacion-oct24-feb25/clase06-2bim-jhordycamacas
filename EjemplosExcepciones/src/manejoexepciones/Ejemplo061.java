4d75f1ac6c2ecfb64f1e1493eeeee3abcf549a7c
jhordycamacas <jhordycamacas9@gmail.com>
2025-01-22 11:58:03 -0500

    cambs

--- EjemplosExcepciones/src/manejoexepciones/Ejemplo061.java
+++ EjemplosExcepciones/src/manejoexepciones/Ejemplo061.java
@@ -13,10 +13,10 @@ import java.util.Scanner;
  *
  * @author reroes﻿​_
  */
-public class Ejemplo06 {
+public class Ejemplo061 {
     
     public static void main(String[] args) {
-        try{
+        //try{
             Scanner entrada = new Scanner(System.in);
             System.out.println("Ingrese valor 1:");
             int valor1 = entrada.nextInt(); // se espera un valor entero
@@ -24,23 +24,12 @@ public class Ejemplo06 {
             int valor2 = entrada.nextInt(); // se espera un valor entero
             int resultado = valor1 / valor2;
             double resultado2 = (double)resultado;
-            System.out.printf("Resultado %.2f", resultado2);
-        }
-        catch (InputMismatchException inputMismatchException) {
-            
-            System.out.printf("Existe un error de tipo %s\n", 
-                    inputMismatchException);
-        }
-        catch(ArithmeticException arithmeticException){
-            
-            System.out.println("Lo sentimos hay un error");
-            System.out.printf("De tipo %s\n", arithmeticException);
-        
-        }
-        catch(IllegalFormatConversionException i){
-            System.out.println("Lo sentimos hay un error");
-            System.out.printf("De tipo %s\n", i);
-        }
+            System.out.printf("Resultado %d", resultado2);
+        //}
+      //  catch(IllegalFormatConversionException i){
+         //   System.out.println("Lo sentimos hay un error");
+         //   System.out.printf("De tipo %s\n", i);
+       // }
                
     }
 }
